688817037899ad2c973940182ecb9dfe02c1bbe6
Jack Jiang <jackjiang871@gmail.com>
2022-04-29 22:17:27 -0700

    highlighting for available moves

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ class Square extends React.Component {
       <button 
         className="square" 
         onClick={() => this.props.onClick() }
+        style={this.props.style}
       >
         {this.props.value}
       </button>
@@ -35,7 +36,25 @@ class Board extends React.Component {
         ['♖', '♘', '♗', '♕', '♔', '♗', '♘', '♖']
       ],
       firstClick: null,
-      isWhitesMove: true
+      isWhitesMove: true,
+      possibleMoves: {
+        "5,0": [[6,0], [7,1]],
+        "4,0": [[6,0]],
+        "5,1": [[6,1]],
+        "4,1": [[6,1]],
+        "5,2": [[6,2], [7,1]],
+        "4,2": [[6,2]],
+        "5,3": [[6,3]],
+        "4,3": [[6,3]],
+        "5,4": [[6,4]],
+        "4,4": [[6,4]],
+        "5,5": [[6,5], [7,6]],
+        "4,5": [[6,5]],
+        "5,6": [[6,6]],
+        "4,6": [[6,6]],
+        "5,7": [[6,7], [7,6]],
+        "4,7": [[6,7]],
+      }
     }
   }
 
@@ -61,6 +80,7 @@ class Board extends React.Component {
 
     console.log(this.state.board)
     console.log(content["next_board"])
+    console.log(content["possible_moves"])
     
     if (JSON.stringify(this.state.board) === JSON.stringify(content["next_board"])) {
       // invalid move
@@ -69,24 +89,50 @@ class Board extends React.Component {
     // valid move
     this.setState({isWhitesMove: isWhitesMove ? false : true})
     this.setState({board: content["next_board"]})
+    var newPossibleMoves = {}
+    for (const move of content["possible_moves"]) {
+      if (!([move[2], move[3]].toString() in newPossibleMoves)) {
+        newPossibleMoves[[move[2], move[3]]] = []
+      }
+      newPossibleMoves[[move[2], move[3]]].push([[move[0], move[1]]])
     }
+    this.setState({possibleMoves: newPossibleMoves})
+    console.log("possible_moves:", content["possible_moves"])
+  }
 
   handleClick(i, j) {
     // first click
     if (!this.state.firstClick) {
       this.setState({firstClick: [i,j]});
+      // show possible moves for this first click
+      this.render()
     } 
     // second click - submit move and reset first click
     else {
-      this.setNextBoard(this.state.firstClick[0], this.state.firstClick[1], i, j, this.state.board, this.state.isWhitesMove);
-      this.setState({firstClick: null});
+      if ([i,j] in this.state.possibleMoves && JSON.stringify(this.state.possibleMoves[[i,j]]).indexOf(JSON.stringify(this.state.firstClick)) !== -1) {
+        this.setNextBoard(this.state.firstClick[0], this.state.firstClick[1], i, j, this.state.board, this.state.isWhitesMove);
+        this.setState({firstClick: null});
+      } else {
+        this.setState({firstClick: [i,j]});
+      }
     }
   }
 
   renderSquare(piece, i, j) {
+    var moves = this.state.possibleMoves[[i,j]]
+    if (this.state.firstClick !== null && moves !== undefined && JSON.stringify(this.state.possibleMoves[[i,j]]).indexOf(JSON.stringify(this.state.firstClick)) !== -1) {
+      return (
+        <Square 
+          value={piece}
+          style={{background: '#fff2f2'}}
+          onClick={() => this.handleClick(i, j)}
+        />
+      );
+    }
     return (
       <Square 
         value={piece}
+        style={{background: '#fff'}}
         onClick={() => this.handleClick(i, j)}
       />
     );
